10051a198bb752e38827f0edea995198655a3650
Gennady Verdel <godrose@gmail.com>
2021-09-23 05:55:06 +0300

    chore: fixed packages names

--- devops/deploy/deploy-all.bat
+++ devops/deploy/deploy-all.bat
@@ -1,14 +1,14 @@
 rem TODO: Use common source for all version instances
 SET version=2.2.3
 rem TODO: Refactor using loop and automatic discovery
-call deploy-single.bat LogoFX.Client.Testing.Contracts %version% 
-call deploy-single.bat LogoFX.Client.Testing.Shared %version%
-call deploy-single.bat LogoFX.Client.Testing.Shared.Caliburn.Micro %version%
-call deploy-single.bat LogoFX.Client.Testing.EndToEnd %version% 
+call deploy-single.bat LogoFX.Client.Tests.Contracts %version% 
+call deploy-single.bat LogoFX.Client.Tests.Shared %version%
+call deploy-single.bat LogoFX.Client.Tests.Shared.Caliburn.Micro %version%
+call deploy-single.bat LogoFX.Client.Tests.EndToEnd %version% 
 call deploy-single.bat LogoFX.Client.Testing.EndToEnd.FlaUI %version% 
 call deploy-single.bat LogoFX.Client.Testing.EndToEnd.White %version% 
 call deploy-single.bat LogoFX.Client.Testing.EndToEnd.SpecFlow %version% 
-call deploy-single.bat LogoFX.Client.Testing.Integration %version% 
-call deploy-single.bat LogoFX.Client.Testing.Integration.NUnit %version% 
+call deploy-single.bat LogoFX.Client.Tests.Integration %version% 
+call deploy-single.bat LogoFX.Client.Tests.Integration.NUnit %version% 
 call deploy-single.bat LogoFX.Client.Testing.Integration.xUnit %version% 
-call deploy-single.bat LogoFX.Client.Testing.Integration.SpecFlow %version%
+call deploy-single.bat LogoFX.Client.Tests.Integration.SpecFlow %version%
