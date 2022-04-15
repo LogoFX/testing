ef739ebb552285f9d1cee6d196185aa36fe96787
Gennady Verdel <godrose@gmail.com>
2022-04-15 09:08:34 +0300

    chore: bump version to 2.2.6

--- devops/deploy/deploy-all.bat
+++ devops/deploy/deploy-all.bat
@@ -1,5 +1,5 @@
 rem TODO: Use common source for all version instances
-SET version=2.2.6-rc2
+SET version=2.2.6
 rem TODO: Refactor using loop and automatic discovery
 call deploy-single.bat LogoFX.Client.Tests.Contracts %version% 
 call deploy-single.bat LogoFX.Client.Tests.Shared %version%
